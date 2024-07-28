069cc8c654fc219425d6607c0d1fe19923b2eed2
CStalks <swiafe.dev@gmail.com>
2024-07-28 00:26:33 -0400

    Fix event targets

--- src/deleteProject.js
+++ src/deleteProject.js
@@ -1,10 +1,9 @@
 function deleteProject(){
-    //deletes the projects from the projects section and updates
-    //those changes from the local storage
     document.addEventListener('click', (e) => {
-        if(e.target.id === 'project-delete-button'){
-            localStorage.removeItem(e.target.parentNode.parentNode.firstChild.textContent.replace("#",'').trim());
-            e.target.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode);
+        if(e.target.className === 'project-delete-btn'){
+            localStorage.removeItem(e.target.parentNode.firstChild.textContent.
+            replace("#",'').trim());
+            e.target.parentNode.parentNode.removeChild(e.target.parentNode);
         }
     });
 }
